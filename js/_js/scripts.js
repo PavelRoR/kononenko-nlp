edad1c597aecb37c8ee84d9c9da580c20660f9fb
pavelp21 <pavelvp2182@yandex.ru>
2019-03-14 20:33:55 +0500

    Section Author is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,6 +1,38 @@
+//@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend owl.carousel.min.js
+//@prepros-prepend jquery.fancybox.min.js
+
+
 $(document).ready(function() {
-
-
-
+    /* Сертификаты */
+	$('#certs_container').owlCarousel({
+        // loop:true,
+        margin:20,
+        nav:true,
+        navText: ["<", ">"],
+        dots: false,
+        responsive:{
+            0:{
+                items:1
+            },
+            400:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+ /*Fancybox*/
+ $(function () {
+    $("[data-fancybox]").fancybox({
+        speed: 330,
+        opacity: "auto",
+        closeBtn: !0
+    });
+});
 /*Конец документа*/
 });
