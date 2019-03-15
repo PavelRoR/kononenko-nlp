889d041341bd21272f056ea0e7f149359895ff1e
pavelp21 <pavelvp2182@yandex.ru>
2019-03-15 11:29:10 +0500

    Section Revs is done

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -3,36 +3,71 @@
 //@prepros-prepend jquery.fancybox.min.js
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     /* Сертификаты */
-	$('#certs_container').owlCarousel({
-        // loop:true,
-        margin:20,
-        nav:true,
+    $('#certs_container').owlCarousel({
+        loop:true,
+        margin: 20,
+        nav: true,
         navText: ["<", ">"],
         dots: false,
-        responsive:{
-            0:{
-                items:1
+        responsive: {
+            0: {
+                items: 1
             },
-            400:{
-                items:2
+            400: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:4
+            1000: {
+                items: 4
             }
         }
     });
- /*Fancybox*/
- $(function () {
-    $("[data-fancybox]").fancybox({
-        speed: 330,
-        opacity: "auto",
-        closeBtn: !0
+    /*Fancybox*/
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            speed: 330,
+            opacity: "auto",
+            closeBtn: !0
+        });
     });
-});
-/*Конец документа*/
+    $(function () {
+        $('#text_revs').owlCarousel({
+            // loop: true,
+            margin: 20,
+            nav: true,
+            navText: ["<", ">"],
+            dots: false,
+            items: 1
+        });
+    });
+    $(function () {
+        $('#video_revs').owlCarousel({
+            // loop: true,
+            margin: 20,
+            nav: true,
+            navText: ["<", ">"],
+            dots: false,
+            items: 2,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                481: {
+                    items: 2
+                }
+            }
+        });
+    });
+    $(function () {
+        /* Видео */
+        $(".video_wrapper img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1" allowfullscreen></iframe>')
+        });
+    });
+    /*Конец документа*/
 });
